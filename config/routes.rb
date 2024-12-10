4aa7483e8b784246633bd9755c1332132c3271e4
Mihir Khandekar <mihir.khandekar91@gmail.com>
2024-12-10 20:27:15 +0530

    feat: add login and logout flows

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'dashboard#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
